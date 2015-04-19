4fb997aa8244e98732114bd58e7fb73930477255
lalaith <salehaiqbalhussain.iitd@gmail.com>
2015-04-19 23:06:38 +0530

    Refined Q3
    Added train-100 case. Added graphs

--- mlas3/q3/gettable.m
+++ mlas3/q3/gettable.m
@@ -9,22 +9,31 @@ l = zeros(2,3);
 
 sizedata = size(data,1);
 
+totalval=0;
 for iter = 1:sizedata
     if data(iter,6)~=0
-    d(data(iter,1)+1,1)=d(data(iter,1)+1,1)+data(iter,6);
-    i(data(iter,2)+1,1)=i(data(iter,2)+1,1)+data(iter,6);
-    g(data(iter,3),(data(iter,1))*2+data(iter,2)+1)=g(data(iter,3),(data(iter,1))*2+data(iter,2)+1)+data(iter,6);
-    s(data(iter,4)+1,data(iter,2)+1) = s(data(iter,4)+1,data(iter,2)+1)+data(iter,6);
-    l(data(iter,5)+1,data(iter,3))=l(data(iter,5)+1,data(iter,3))+data(iter,6);  
+        totalval=totalval+1;
+        d(data(iter,1)+1,1)=d(data(iter,1)+1,1)+data(iter,6);
+        i(data(iter,2)+1,1)=i(data(iter,2)+1,1)+data(iter,6);
+        g(data(iter,3),(data(iter,1))*2+data(iter,2)+1)=g(data(iter,3),(data(iter,1))*2+data(iter,2)+1)+data(iter,6);
+        s(data(iter,4)+1,data(iter,2)+1) = s(data(iter,4)+1,data(iter,2)+1)+data(iter,6);
+        l(data(iter,5)+1,data(iter,3))=l(data(iter,5)+1,data(iter,3))+data(iter,6);  
     end
 end
 
-
-d = bsxfun(@rdivide,d,sum(d));
-i = bsxfun(@rdivide,i,sum(i));
-g = bsxfun(@rdivide,g,sum(g));
-s = bsxfun(@rdivide,s,sum(s));
-l = bsxfun(@rdivide,l,sum(l));
+if totalval~=0
+    d = bsxfun(@rdivide,d,sum(d));
+    i = bsxfun(@rdivide,i,sum(i));
+    g = bsxfun(@rdivide,g,sum(g));
+    s = bsxfun(@rdivide,s,sum(s));
+    l = bsxfun(@rdivide,l,sum(l)); 
+else
+    d = [0.5;0.5];
+    i = [0.5;0.5];
+    g = [0.25 0.25 0.25 0.25;0.25 0.25 0.25 0.25;0.25 0.25 0.25 0.25];
+    s = [0.5 0.5;0.5 0.5];
+    l = [0.25 0.5 0.5;0.5 0.25 0.25;0.25 0.25 0.5];
+end
 
 
 end
--- mlas3/q3/p2.m
+++ mlas3/q3/p2.m
@@ -1,6 +1,6 @@
 
 %reading the data
-data = getdata('q3_data/train-80.data');
+data = getdata('q3_data/train-100.data');
 
 %creating the data table
 [d,i,g,s,l ] = gettable( data );
@@ -13,7 +13,6 @@ forplot = [];
 
 while(error~=0)
     iteration = iteration+1
-    forplot = [forplot;[error,p]];
     %Expectation - add to the data
     datanew = expectation( d,i,g,s,l,data );
     
@@ -29,15 +28,19 @@ while(error~=0)
     newp = probtest(d,i,g,s,l);
     p = newp;
     error = p-oldp;
+    
+    forplot = [forplot;[error,p]];
+    error
+    p
 end
 
 d = size(forplot,1);
 figure % create new figure
 subplot(1,2,1) % first subplot
-plot(forplot(3:d,1));
+plot(forplot(2:d,1));
 title('Error');
 subplot(1,2,2)
-plot(forplot(3:d,2),'r');
+plot(forplot(2:d,2),'r');
 title('Prob');
 
 
